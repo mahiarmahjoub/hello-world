72a65d39f07fb680ad0aaab9ec57f88b4b1ddbe9
mahiarmahjoub <31858737+mahiarmahjoub@users.noreply.github.com>
2017-09-13 15:25:33 +0100

    Update learn-python.py

--- learn-python.py
+++ learn-python.py
@@ -68,3 +68,64 @@ arr_pos.std()
 
 arr_pos.sort()  # if arr_pos needs to be preserved, np.copy into a new vector and sort the latter
 
+# array input and output 
+arr = np.arange(5)
+np.save('my_array',arr)   # saves as a .npy file 
+np.load('my_array.npy')
+
+arr = np.array([[1,2,3],[4,5,6]])
+np.savetxt('my_test_text.txt',arr,delimiter=',')
+arr = np.loadtxt('my_test_text.txt',delimiter = ',')
+
+# Pandas 
+
+import pandas as pd
+from pandas import Series,DataFrame   # since we use Series, DataFrame a lot, we import them directly as opposed to np.Series every time
+
+ww2_cas = Series([8700000,4300000,3000000,2100000,400000],index=['USSR','Germany','China','Japan','USA'])
+ww2_cas.values   # returns the scalars only 
+ww2_cas.index    # returns the labels for each value 
+ww2_cas['USA']   # calls the value for the corresponding index of the entry 
+ww2_cas[ww2_cas > 400000].index    # show which countries had more than 400000 casualties 
+ww2_cas.name = "World War Two Casualties"
+# you can also add Series together e.g. ww2_cas + ww2_cas will add the values 
+ww2_cas.values.name = "Casualties"    # names the indices 
+
+DataFrame(nfl_frame,columns=['Team','First Season','Total Games'])   # select only the said columns from the nfl_frame table
+dframe = DataFrame(randn(25).reshape((5,5)),index=['A','B','D','E','F'],columns=['col1','col2','col3','col4','col5'])
+
+dframe2 = dframe.reindex(['A','B','C','D','E','F'])    # adds an empty row 
+new_columns = ['col1','col2','col3','col4','col5','col6']
+dframe2.reindex(columns=new_columns)    # adds a new empty column 
+
+# adds those names to the 1st and 5th row in 'Stadium column'
+nfl_frame['Stadium'] = Series(["Levi's Stadium","AT&T Stadium"],index=[4,0]) 
+
+# constructing dataframe from scratch - another way 
+data = {'City':['SF','LA','NYC'],
+        'Population':[837000,3880000,8400000]}
+city_frame = DataFrame(data)
+
+# if we have a DataFrame matrix named 'frame', then
+frame.sort()
+frame.mean()
+frame.sum()    # takes the sum of each column 
+frame.sum(axis=1)   # takes the sum of each row 
+frame.describe()   # provides summary statistics for each column 
+frame.plot()   # plots each column against the indices 
+
+# filling or removing 'NA' or empty entries in a database
+frame.isnull()   # returns a boalean of all the empty entries 
+frame.dropna()   # gets rid of every row containing at least on NA
+frame.dropna(how='all')   # only removes row where all entries is NA
+frame.dropna(thresh=2)  # only removes rows with more than 2 NA
+frame.fillna(1)   # fill all the NA with 1 
+
+ser = Series(np.random.randn(6),index=[[1,1,1,2,2,2],['a','b','c','a','b','c']])   # multiple index levels 
+dframe = ser.unstack()   # turns the multi-index series into a table 
+
+frame = pd.read_csv('filename.csv')    # read 'filename.csv' into a DataFrame named frame 
+frame = pd.read_table('filename.csv',sep=',')  # if a different separater is used 
+frame.to_csv('mytextdata_out.csv')    # export 'frame' as a csv file 
+
+ 
